995b7629a7bfaa83e1b6277e6c7c06d673183491
dhnguyen <nguyen.h.david@gmail.com>
2016-04-24 21:24:14 -0400

    added GPS, not functional yet

--- app/src/main/java/edu/umass/cs/crowdpark/DataInfo/ParkingType.java
+++ app/src/main/java/edu/umass/cs/crowdpark/DataInfo/ParkingType.java
@@ -3,6 +3,7 @@ package edu.umass.cs.crowdpark.dataInfo;
 /**
  * Created by Tommy on 4/13/2016.
  */
+
 public abstract class ParkingType {
     public String name;
 
--- app/src/main/java/edu/umass/cs/crowdpark/MainActivity.java
+++ app/src/main/java/edu/umass/cs/crowdpark/MainActivity.java
@@ -1,21 +1,33 @@
 package edu.umass.cs.crowdpark;
 
+import android.Manifest;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.firebase.client.Firebase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener{
 
     SharedPreferences pref;
 
     private static String CONSUMER_KEY = "cvdZx83aZX3n8Ox6U6dvhjJ77";
     private static String CONSUMER_SECRET = "IXvdcDg8W5Q0gAig9IC1ltWzJHsdpAqq2xkongJnDoRJoxS0eB";
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    private Location currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,20 @@ public class MainActivity extends AppCompatActivity {
         edit.putString("CONSUMER_KEY", CONSUMER_KEY);
         edit.putString("CONSUMER_SECRET", CONSUMER_SECRET);
         edit.commit();
+
+        //Location stuff
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (locationManager != null) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                    || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                locationManager.removeUpdates(this);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+                Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                double longitude = location.getLongitude();
+                double latitude = location.getLatitude();
+                Log.v("location", "Latitude:" + currentLocation.getLatitude() + ", Longitude:" + currentLocation.getLongitude());
+            }
+        }
 
         //Firebase stuff
         Firebase.setAndroidContext(this);
@@ -38,6 +64,29 @@ public class MainActivity extends AppCompatActivity {
         ft.addToBackStack(null);
         ft.commit();
     }
+
+    @Override
+    public void onLocationChanged(Location location){
+        currentLocation = location;
+        //Log.v("location", "Latitude:" + currentLocation.getLatitude() + ", Longitude:" + currentLocation.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider){
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider){
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras){
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -60,5 +109,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
 
